7d47ded948d137abbd412527aec91bbe705d65b6
abdalrhmanAlkraien <abdalrhman.alkraien@cirrusgo.com>
2024-02-21 23:34:58 +0200

    multi module project with layer

--- Multi module/app-module/src/main/java/com/project/multimodule/MultiModuleApplication.java
+++ Multi module/app-module/src/main/java/com/project/multimodule/MultiModuleApplication.java
@@ -3,7 +3,7 @@ package com.project.multimodule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.project.multimodule")
 public class MultiModuleApplication {
 
     public static void main(String[] args) {
